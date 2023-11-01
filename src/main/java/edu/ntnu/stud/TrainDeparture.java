1de1f8300887b01a304fd1c384a09a4b537eaad1
SM <st.mockus@gmail.com>
2023-11-01 15:18:37 +0100

    Full javadoc for TrainDeparture class

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -5,12 +5,12 @@ import java.time.LocalTime;
 import java.util.regex.Pattern;
 
 /**
- * A class that represents a train departure.
+ * Represents a train departure with specific details such as departure time, line, destination,
+ * train number, track number, and delay.
  */
 public class TrainDeparture {
 
-
-  private LocalTime departureTime;
+  private final LocalTime departureTime;
   private final String line;
   private final String destination;
   private final int trainNumber;
@@ -19,15 +19,19 @@ public class TrainDeparture {
   private static final Pattern timePattern = Pattern.compile("([01]?[0-9]|2[0-3]):[0-5][0-9]");
 
 
+
   /**
-   * @param departureTime The time of departure.
-   * @param line The line the train is on.
+   * Constructs a TrainDeparture object with the specified departure time, line, destination,
+   * train number, track number, and delay.
+   *
+   * @param departureTime The departure time of the train in the format HH:mm.
+   * @param line The line on which the train is operating.
    * @param destination The destination of the train.
-   * @param trainNumber The train number.
-   * @param track The track the train is on.
-   * @param delay The delay of the train.
-   * @throws DateTimeException if the departure time or delay is not in the format HH:mm.
-   * @throws NullPointerException if the destination is null.
+   * @param trainNumber The unique identifier of the train.
+   * @param track The track number from which the train departs (-1 if unknown).
+   * @param delay The delay time in minutes (in the format HH:mm) if the train is delayed.
+   * @throws DateTimeException If the departure time or delay format is invalid.
+   * @throws NullPointerException If the destination is null.
    */
   TrainDeparture(LocalTime departureTime, String line, String destination,
           int trainNumber, int track, LocalTime delay) {
@@ -49,39 +53,64 @@ public class TrainDeparture {
   }
 
   /**
-   * Returns noe...
+   * Constructs a TrainDeparture object with the specified departure time, line, destination,
+   * train number, and delay. The track number is set to -1 as it is unknown.
    *
-   * @param departureTime The time of departure
-   * @param line The line the train is on
-   * @param destination The destination of the train
-   * @param trainNumber The train number
-   * @param delay The delay of the train
+   * @param departureTime The departure time of the train in the format HH:mm.
+   * @param line The line on which the train is operating.
+   * @param destination The destination of the train.
+   * @param trainNumber The unique identifier of the train.
+   * @param delay The delay time in minutes (in the format HH:mm) if the train is delayed.
+   * @throws DateTimeException If the departure time or delay format is invalid.
+   * @throws NullPointerException If the destination is null.
    */
   TrainDeparture(LocalTime departureTime, String line, String destination,
                  int trainNumber, LocalTime delay) {
     this(departureTime, line, destination, trainNumber, -1, delay);
   }
 
-
+  /**
+   * Gets the departure time of the train.
+   *
+   * @return The departure time of the train.
+   */
   public LocalTime getDepartureTime() {
     return departureTime;
   }
 
+
+  /**
+   * Gets the line on which the train is operating.
+   *
+   * @return The line of the train.
+   */
   public String getLine() {
     return line;
   }
 
+  /**
+   * Gets the destination of the train.
+   *
+   * @return The destination of the train.
+   */
   public String getDestination() {
     return destination;
   }
 
+  /**
+   * Gets the unique identifier of the train.
+   *
+   * @return The train number.
+   */
   public int getTrainNumber() {
     return trainNumber;
   }
 
   /**
-   * @param track The track the train is on.
-   * @throws IllegalArgumentException if the track is less than -1.
+   * Sets the track number for the train.
+   *
+   * @param track The track number from which the train departs.
+   * @throws IllegalArgumentException If the track number is less than or equal to 0.
    */
   public void setTrack(int track) {
     if (track <= 0) {
@@ -92,31 +121,52 @@ public class TrainDeparture {
   }
 
   /**
-   * @param delay The delay of the train.
-   * @throws DateTimeException if the delay is not in the format HH:mm.
+   * Sets the delay time for the train.
+   *
+   * @param delay The delay time in HH:mm.
+   * @throws DateTimeException If the delay time format is invalid.
    */
   public void setDelay(LocalTime delay) {
-    if (delay == LocalTime.of(0, 0)) {
-      throw new DateTimeException("Delay cannot be null");
+    if (!timePattern.matcher(delay.toString()).matches()) {
+      throw new DateTimeException("Delay must be in the format HH:mm");
+    } else {
+      this.delay = delay;
     }
-    this.delay = delay;
   }
 
+  /**
+   * Gets the track number from which the train departs.
+   *
+   * @return The track number.
+   */
   public int getTrack() {
     return track;
   }
 
+  /**
+   * Gets the delay time of the train in minutes.
+   *
+   * @return The delay time of the train.
+   */
   public LocalTime getDelay() {
     return delay;
   }
 
+  /**
+   * Gets the departure time of the train considering the delay.
+   *
+   * @return The departure time with delay taken into account.
+   */
   public LocalTime getDepartureTimeWithDelay() {
     return departureTime.plusHours(delay.getHour()).plusMinutes(delay.getMinute());
   }
 
-
   /**
-   *  Returns a string representation of the train departure.
+   * Returns a formatted string representation of the TrainDeparture object.
+   * The string contains departure time, destination, track number, line, train number,
+   * and delay, separated by vertical bars ('|') and padded for alignment.
+   *
+   * @return A formatted string representing the TrainDeparture object's details.
    */
   public String toString() {
     StringBuilder info = new StringBuilder();
@@ -129,5 +179,4 @@ public class TrainDeparture {
     info.append(delay).append(String.format("%" + 7 + "s", "|")).append("\n");
     return info.toString();
   }
-
 }
